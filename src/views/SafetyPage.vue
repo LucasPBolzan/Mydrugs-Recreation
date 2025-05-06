7d358a18ff184d66d087dba8c6b2565bc3530f92
Wagner Salbego <105342@urisantiago.br>
2025-05-06 14:32:30 -0300

    Criação da SafetyPage

--- src/views/SafetyPage.vue
+++ src/views/SafetyPage.vue
@@ -1,13 +1,46 @@
 <script setup>
-
 </script>
 
 <template>
+  <h1></h1>
   <div class="page">
-    <h1>pagina</h1>
+    <div class="faq-box">
+      <h2>MDMA SAFETY GUIDE</h2>
+      <p><strong>Bond:</strong> 3,4-Methylendioxy-N-methlamphetamine</p>
+      <p>
+        When taking MDMA, it is recommended not to exceed.
+        maximum dose of 1.5 mg MDMA per kg body weight for men and 1.3 mg MDMA per kg body weight for women. 
+        In general, a dose of 120 mg should not be exceeded. 
+        After ingestion, be sure to hydrate sufficiently. 
+        Only obtain MDMA from trustworthy sources.
+      </p>
+      <p><strong>*Information not guaranteed</strong><br>
+        More info: <a href="https://en.wikipedia.org/wiki/MDMA" target="_blank">Wikipedia info</a>.
+      </p>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.page {
+  padding: 20px;
+  font-family: Arial, sans-serif;
+  color: #fff;
+  background-color: #121212;
+  min-height: 100vh;
+  margin-top: 100px;
+}
 
+.faq-box {
+  background-color: rgba(255, 255, 255, 0.05);
+  padding: 20px;
+  border-radius: 20px;
+  border: 2px solid white;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.faq-box a {
+  color: #ff9100;
+}
 </style>
