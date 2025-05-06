ad61f516d89733c8198800937ee8c019674ac24c
Wagner Salbego <105342@urisantiago.br>
2025-05-06 13:55:04 -0300

    Criação da pagina FAQ

--- src/views/FaqPage.vue
+++ src/views/FaqPage.vue
@@ -1,13 +1,57 @@
 <script setup>
-
 </script>
 
 <template>
+  <h1></h1>
   <div class="page">
-    <h1>pagina</h1>
+    <div class="faq-box">
+      <p><strong>Is my purchase completely secure through your shop when I make a Purchase?</strong><br>
+      Yes. Just add the desired quantity to the cart and enter your address at the checkout. Your personal data is automatically encrypted using PGP and deleted after the order is dispatched. Our team of professionals then packs the goods in clean-room conditions, and ensures smooth and secure processing.</p>
+
+      <p><strong>Can I really only pay with cryptocurrency?</strong><br>
+      Yes. We only accept payments in Bitcoin, Ripple, IOTA, and Ethereum. The payment procedure is not complicated. You just need to set up a suitable wallet. We do not offer Payback or other bonus systems. We weren’t born yesterday.</p>
+
+      <p><strong>How can I receive my order anonymously?</strong><br>
+      Our shipping department operates around the clock to ensure fast and risk-free shipping. The delivery usually arrives within a few days. Your order can be sent to any valid postal address, PO box, or package station. Thanks to our highly complex and decentralized system, the transfer to the logistics service provider is already anonymous and cannot be traced back.</p>
+
+      <p><strong>Is your product safe?</strong><br>
+      Nobody has claimed that doing drugs was safe. However as long as you follow the guide in the Safety section there should not be any issues. Do not take MDMA if you are also under any prescriptions or doing any other drugs. It is also recommended not to take MDMA with any sort of alcohol as it might cause irreversible damage to your immune system and overall health.</p>
+
+      <p><strong>Is any of this real?</strong><br>
+      No. Of course not.</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.page {
+  padding: 20px;
+  font-family: Arial, sans-serif;
+  color: #fff;
+  background-color: #121212;
+  min-height: 100vh;
+  margin-top: 100px;
+}
+h1 {
+  margin-bottom: 20px;
+  font-size: 32px;
+}
 
+.faq-box {
+  background-color: #1e1e1e;
+  border-radius: 12px;
+  padding: 20px;
+  border: 2px solid #ffffff; 
+}
+
+.faq-box p {
+  margin-bottom: 20px;
+  line-height: 1.5;
+}
+
+.faq-box strong {
+  display: block;
+  margin-bottom: 5px;
+  font-weight: bold;
+}
 </style>
